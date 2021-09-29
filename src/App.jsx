e7698b2a5c8f1e4550c20f481181a816c96599cc
Jose J. Cruz <jjzcru@gmail.com>
2021-09-29 12:39:25 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import styles from './app.module.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { AppContext } from './services/AppContext';
+import ReloadPrompt from './ReloadPrompt';
+
+import Home from './pages/home';
+
+export class App extends Component {
+	state = {};
+	componentDidMount() {
+		
+	}
+
+	render() {
+		return (
+			<AppContext.Provider
+				value={{
+				}}
+			>
+				<main className={styles['app']}>
+					<Router>
+						<Switch>
+							<Route exact path="/" component={Home} />
+						</Switch>
+					</Router>
+				</main>
+				<ReloadPrompt />
+			</AppContext.Provider>
+		);
+	}
+}
+
+function uniqueArticles(articles) {
+	const articleMap = {};
+	let articleOrder = [];
+	const response = [];
+	for (let article of articles) {
+		articleMap[`${article.id}`] = article;
+		articleOrder.push(article.id);
+	}
+
+	articleOrder = [...new Set(articleOrder)];
+	for (let id of articleOrder) {
+		response.push(articleMap[id]);
+	}
+
+	return response.filter((r) => !!r);
+}
---
+++ src/pages/home.jsx
@@ -0,0 +1,6 @@
+import React, { useContext, useEffect, useState } from 'react';
+import styles from './home.module.css';
+
+export default function Home() {
+	return <div>Home</div>;
+}
