34604ba398bd17340ee4c77cdab802369eeee84e
Jose J. Cruz <jjzcru@gmail.com>
2021-10-06 13:30:58 -0400

    test: :white_check_mark: Add support for coverage

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
-module.exports = {
-    presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
-  };
+module.exports = function (api) {
+	const presets = [
+		'@babel/preset-env',
+		'@babel/preset-react',
+		'@babel/preset-flow',
+	];
+	const plugins = ['@babel/plugin-transform-runtime'];
+
+	/** this is just for minimal working purposes,
+	 * for testing larger applications it is
+	 * advisable to cache the transpiled modules in
+	 * node_modules/.bin/.cache/@babel/register* */
+	api.cache(false);
+
+	return {
+		presets,
+		plugins,
+	};
+};
