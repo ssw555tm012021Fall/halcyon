b0f404e15ad95e46be500de94338561a092c9a8b
Jose J. Cruz <jjzcru@gmail.com>
2021-10-23 18:01:01 -0400

    feat: :sparkles: Add events to OpenApi spec

--- src/pages/settings.jsx
+++ src/pages/settings.jsx
@@ -1,5 +1,5 @@
-import React, { useContext, useEffect } from 'react';
-import styles from './home.module.css';
+import React, { useContext, useEffect, useState } from 'react';
+import styles from './settings.module.css';
 import { BottomBar } from '../components/BottomBar';
 import { AppContext } from '../services/AppContext';
 import { Redirect } from 'react-router';
@@ -12,23 +12,88 @@ export default function Settings() {
 	return (
 		<div className={'view'}>
 			<header>Settings</header>
-			<main
-				style={{
-					display: 'flex',
-					justifyContent: 'center',
-					alignItems: 'center',
-				}}
-			>
-				<button
-					onClick={() => {
-						localStorage.removeItem('token');
-						window.location = '/';
-					}}
-				>
-					Log out
-				</button>
+			<main className={styles['settings']}>
+				<div>
+					<NotificationOption />
+					<NoticationDetails />
+					<button
+						className={styles['logout']}
+						onClick={() => {
+							/*localStorage.removeItem('token');
+							window.location = '/';*/
+						}}
+					>
+						Log out
+					</button>
+				</div>
 			</main>
 			<BottomBar selected={'settings'} />
 		</div>
+	);
+}
+
+function NotificationOption() {
+	const [isChecked, setIsChecked] = useState(
+		Notification?.permission === 'granted'
+	);
+	const onToggle = (e) => {
+		const value = e.target.checked;
+		if (value) {
+			Notification.requestPermission().then(function (permission) {
+				if (permission === 'granted') {
+					setIsChecked(true);
+				} else {
+					setIsChecked(false);
+				}
+			});
+		} else {
+			if (Notification?.permission === 'granted') {
+				setIsChecked(true);
+			} else {
+				setIsChecked(false);
+			}
+		}
+	};
+	return (
+		<div className={styles['options']}>
+			<div className={styles['option']}>
+				<div>
+					<span>Notifications </span>
+					<Toggle on={isChecked} onToggle={onToggle} />
+				</div>
+			</div>
+		</div>
+	);
+}
+
+function NoticationDetails() {
+	return (
+		<div className={styles['options']} style={{
+			marginTop: 20
+		}}>
+			<div className={styles['option']}>
+				<div>
+					<span>Water </span>
+					<span>details</span>
+				</div>
+			</div>
+			<div className={styles['option']}>
+				<div>
+					<span>Break </span>
+					<span>details</span>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+function Toggle({ on, onToggle }) {
+	return (
+		<label className={styles['switch']}>
+			<input type="checkbox" checked={on} onChange={onToggle} />
+			<span
+				className={[styles['slider'], styles['round']].join(' ')}
+			></span>
+		</label>
 	);
 }
